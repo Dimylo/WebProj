a36e9f9c50dc173435f9a497d6801616631cd53d
Orestis Papanikolaou <papanikolaou.orestis@gmail.com>
2020-11-08 19:05:57 +0200

    checkmainuserUnchained

--- user/checkmainuser1.php
+++ user/checkmainuser1.php
@@ -3,8 +3,4 @@ session_start();
 
 $db = pg_connect("host=localhost dbname=mydb user=postgres password=root");
 
-if(!isset($_SESSION['id'])){ //if login in session is not set
-    header("Location: http://www.example.com/login.php");
-}
-
 ?>
